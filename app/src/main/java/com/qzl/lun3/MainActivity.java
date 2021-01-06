5846bff76c2278c039b36be823b620aac40a2335
qiaozhiling <1890509733@163.com>
2021-01-06 10:54:07 +0800

    1.6.2

--- app/src/main/java/com/qzl/lun3/MainActivity.java
+++ app/src/main/java/com/qzl/lun3/MainActivity.java
@@ -3,6 +3,7 @@ package com.qzl.lun3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //设置窗体为没有标题的模式
+
     }
 }
