e301da832958fde2936c462d3f1c899b199dc9a8
qiaozhiling <1890509733@163.com>
2021-01-07 15:48:33 +0800

    1.7.2

--- app/src/main/java/com/qzl/lun3/DetailActivity.java
+++ app/src/main/java/com/qzl/lun3/DetailActivity.java
@@ -2,13 +2,35 @@ package com.qzl.lun3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    Button button;
+    ImageView headPicDetail;
+    TextView groupNameDetail,creatorNameDetail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent intent=getIntent();
+
+
+
+        button = findViewById(R.id.sure);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/qzl/lun3/MainActivity.java
+++ app/src/main/java/com/qzl/lun3/MainActivity.java
@@ -8,26 +8,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
-    ArrayList<Integer> headPic;//头像
-    ArrayList<String> groupName = new ArrayList<>();//群名
-    ArrayList<String> creator = new ArrayList<>();//群聊创造者
-    Adapter adapter;//适配器
+    Data data;//数据
+    RecyclerAdapter adapter;//适配器
 
     /////////////////////// //////onCreate/////////////////////////////////////
     @Override
@@ -35,25 +28,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        headPic = new ArrayList<>();//头像
-        groupName = new ArrayList<>();//群名
-        creator = new ArrayList<>();//群聊创造者
-
-        headPic.add(R.drawable.head1);
-        headPic.add(R.drawable.head2);
-        headPic.add(R.drawable.head3);
-        headPic.add(R.drawable.head4);
-
-        groupName.add("Agroup");
-        creator.add("A");
-
+        data = new Data();
 
         recyclerView = findViewById(R.id.recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new Adapter(headPic, groupName, creator);
+
+        adapter = new RecyclerAdapter(this, data);
         recyclerView.setAdapter(adapter);
 
+        adapter.setOnItemClickListener(new RecyclerAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View v) {
+                toDetail(v);
+            }
+        });
+    }
 
+    /////////////////////////////Detail///////////////////////////////////////
+    public void toDetail(View v) {
+
+        ImageView headPic = v.findViewById(R.id.headPic);
+        TextView groupName = v.findViewById(R.id.groupName);
+        TextView creator = v.findViewById(R.id.creator);
+
+
+        Bundle bundle = new Bundle();
+
+
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
     }
 
     //////////////////////////////Menu////////////////////////////////////////
@@ -82,87 +86,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void add() {
-        if (creator.size() >= 4 || groupName.size() >= 4) {
+        /*if (data.creator.size() >= 4 || data.groupName.size() >= 4) {
             Toast.makeText(this, "群聊数量上限", Toast.LENGTH_SHORT).show();
             return;
-        }
+        }*/
 
         Intent intent = new Intent(this, AddActivity.class);
         startActivityForResult(intent, 0);
     }
 
     private void clear() {
-        creator.clear();
-        groupName.clear();
+        data.creator.clear();
+        data.groupName.clear();
         adapter.notifyDataSetChanged();
     }
 
     ////////////////////////////onActivityResult///////////////////////////////
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent dataIntent) {
+        super.onActivityResult(requestCode, resultCode, dataIntent);
 
-        if (data == null || groupName.contains(data.getExtras().getString("GroupName"))) {
+        if (dataIntent == null) {
             Toast.makeText(this, "添加失败", Toast.LENGTH_SHORT).show();
+        } else if ((data.groupName).contains(dataIntent.getExtras().getString("GroupName"))) {
+            Toast.makeText(this, "添加失败,群聊已存在", Toast.LENGTH_SHORT).show();
         } else {
-            groupName.add(data.getExtras().getString("GroupName"));
-            creator.add(data.getExtras().getString("Creator"));
+            data.groupName.add(dataIntent.getExtras().getString("GroupName"));
+            data.creator.add(dataIntent.getExtras().getString("Creator"));
             adapter.notifyDataSetChanged();
-            Toast.makeText(this, "创建成功", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "群聊创建成功", Toast.LENGTH_SHORT).show();
         }
 
-    }
-
-    /////////////////////// //////Adapter/////////////////////////////////////
-    class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
-
-
-        List<Integer> headPic;//头像
-        List<String> groupName;//群名
-        List<String> creator;//群聊创造者
-
-
-
-        public Adapter(List<Integer> headPic, List<String> groupName, List<String> creator) {
-            this.headPic = headPic;
-            this.groupName = groupName;
-            this.creator = creator;
-        }
-
-        @NonNull
-        @Override
-        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-            return new ViewHolder(LayoutInflater.from(MainActivity.this).inflate(R.layout.recycler_, null));
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-            holder.headPic.setImageResource(headPic.get(groupName.size() - position - 1));
-            holder.groupName.setText(groupName.get(groupName.size() - position - 1));
-            holder.creator.setText(creator.get(creator.size() - position - 1));
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return groupName.size();
-        }
-
-        class ViewHolder extends RecyclerView.ViewHolder {
-
-            ImageView headPic;
-            TextView creator;
-            TextView groupName;
-
-            public ViewHolder(@NonNull View itemView) {
-                super(itemView);
-                headPic = itemView.findViewById(R.id.headPic);
-                creator = itemView.findViewById(R.id.creator);
-                groupName = itemView.findViewById(R.id.groupName);
-
-            }
-        }
     }
 
 }
